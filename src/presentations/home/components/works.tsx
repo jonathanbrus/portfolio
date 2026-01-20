79f7891a72e045ec4938d18b46a8976786f12e5c
jonathanbrus26 <jonathanbrus26@gmail.com>
2026-01-21 00:51:24 +0400

    fix mui timeline view bug

--- src/presentations/home/components/works.tsx
+++ src/presentations/home/components/works.tsx
@@ -1,5 +1,4 @@
-import { Box, Typography, Container } from "@mui/material";
-
+import { Typography, Container, Stack } from "@mui/material";
 import Timeline from "@mui/lab/Timeline";
 import TimelineItem from "@mui/lab/TimelineItem";
 import TimelineSeparator from "@mui/lab/TimelineSeparator";
@@ -11,7 +10,7 @@ import { jobExperiences } from "../../../_data/experiences";
 
 export const Works: React.FC = () => {
   return (
-    <Box component="div" id="works">
+    <Stack component="div" id="works">
       <Container
         sx={{
           px: { xs: "1.6rem", sm: "2.6rem" },
@@ -24,49 +23,46 @@ export const Works: React.FC = () => {
         <Typography sx={{ my: "0.3rem", fontSize: { xs: "2.2rem", sm: "2.6rem" }, fontWeight: "bold" }}>
           Works
         </Typography>
+
         <JobExperiences />
-        {/* <Box sx={{ mt: "3rem" }}>
-          <Typography sx={{ my: "0.3rem", fontSize: "2rem", fontWeight: "bold" }}>Featured Projetcs</Typography>
-          <Box>
-          {projects.map((project, i) => {
-            return <Project project={project} key={i} />;
-          })}
-        </Box>
-        </Box> */}
       </Container>
-    </Box>
+    </Stack>
   );
 };
 
-const JobExperiences = () => {
+const JobExperiences: React.FC = () => {
   return (
-    <Timeline position="alternate" sx={{ p: 0 }}>
-      {jobExperiences.map((experience, i) => {
-        return (
-          <TimelineItem key={i}>
-            <TimelineOppositeContent>
-              <Typography {...(i === 0 && { color: "primary.light" })} variant="caption">
-                {experience.duration}
-              </Typography>
-            </TimelineOppositeContent>
-            <TimelineSeparator>
-              <TimelineDot {...(i === 0 && { color: "primary" })} />
-              <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent sx={{ my: "1.3rem" }}>
-              <Typography sx={{ fontSize: { xs: "1.4rem", sm: "1.8rem", md: "2.2rem" }, fontWeight: "bold" }}>
-                {experience.company}
-              </Typography>
-              <Typography variant="subtitle2" sx={{ color: "grey", my: "0.2rem" }}>
-                {experience.designation}
-              </Typography>
-              <Typography variant="body1" sx={{ mt: "1rem" }}>
-                {experience.responsibilities}
-              </Typography>
-            </TimelineContent>
-          </TimelineItem>
-        );
-      })}
-    </Timeline>
+    <Stack>
+      <Timeline position="alternate">
+        {jobExperiences.map((experience, i) => {
+          return (
+            <TimelineItem key={experience.duration} sx={{ "::before": { content: "none" } }}>
+              <TimelineOppositeContent>
+                <Typography {...(i === 0 && { color: "primary.light" })} variant="caption">
+                  {experience.duration}
+                </Typography>
+              </TimelineOppositeContent>
+
+              <TimelineSeparator>
+                <TimelineDot {...(i === 0 && { color: "primary" })} />
+                <TimelineConnector />
+              </TimelineSeparator>
+
+              <TimelineContent sx={{ my: "1.3rem" }}>
+                <Typography sx={{ fontSize: { xs: "1.4rem", sm: "1.8rem", md: "2.2rem" }, fontWeight: "bold" }}>
+                  {experience.company}
+                </Typography>
+                <Typography variant="subtitle2" sx={{ color: "grey", my: "0.2rem" }}>
+                  {experience.designation}
+                </Typography>
+                <Typography variant="body1" sx={{ mt: "1rem" }}>
+                  {experience.responsibilities}
+                </Typography>
+              </TimelineContent>
+            </TimelineItem>
+          );
+        })}
+      </Timeline>
+    </Stack>
   );
 };
